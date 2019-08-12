26d0065195e6129704a7de810f150cb22fb36428
Jesse Khorasanee <jessekhorasanee@gmail.com>
2019-08-12 17:13:04 +1200

    Removed simui from plotsvy.js and reformatted

--- js/plotsvy.js
+++ js/plotsvy.js
@@ -25,12 +25,12 @@ function Plotsvy(targetDiv) {
   _this.sim = undefined
 
   // Assume default HTML is used if none is provided
-  if (targetDiv === null || targetDiv === undefined){
+  if (targetDiv === null || targetDiv === undefined) {
     parentDiv = document.getElementById('plotsvy-example-panel')
   } else {
     parentDiv = targetDiv
   }
-  
+
 
   // initialise: sets up ui and plot, needs DOM to be loaded
   this.initialise = function () {
@@ -42,25 +42,25 @@ function Plotsvy(targetDiv) {
     _this.sim = new SimProcessor(parentDiv, plot)
   }
 
-  this.openBroadcastChannel = function(name){
+  this.openBroadcastChannel = function (name) {
     bc.close()
     bc = new BroadcastChannel.default(name)
   }
 
-  this.sendChannelMessage = function(message){
+  this.sendChannelMessage = function (message) {
     bc.postMessage(message)
   }
 
   // csvChannelCall: Add trace to plot from the choices.js select box
-  var csvChannelCall = function(){
+  var csvChannelCall = function () {
     var selectedChannel = parentDiv.querySelector('#select_channel').textContent
-    plot.addDataSeriesToChart(csv.getColoumnByName(selectedChannel),csv.getColoumnByIndex(0), selectedChannel)
+    plot.addDataSeriesToChart(csv.getColoumnByName(selectedChannel), csv.getColoumnByIndex(0), selectedChannel)
     state.selectedChannels.push(selectedChannel)
-    bc.postMessage({'state': _this.exportStateAsString()})
+    bc.postMessage({ 'state': _this.exportStateAsString() })
   }
 
   // checkBoxCall: Function to pass to dat.gui to add trace to plot
-  var checkBoxCall = function(channel, index, flag){
+  var checkBoxCall = function (channel, index, flag) {
     if (!flag) { // Flag lets us know if checkbox was checked before this click
       plot.addDataSeriesFromDatGui(csv.getColoumnByIndex(index), csv.getColoumnByIndex(0), channel, index)
       state.selectedChannels.push(channel)
@@ -68,30 +68,30 @@ function Plotsvy(targetDiv) {
     else {
       plot.removeSeries(index)
       ch_ind = state.selectedChannels.indexOf(channel)
-      state.selectedChannels.splice( ch_ind, ch_ind + 1) // Remove channel index
+      state.selectedChannels.splice(ch_ind, ch_ind + 1) // Remove channel index
     }
-    bc.postMessage({'state': _this.exportStateAsString()})
+    bc.postMessage({ 'state': _this.exportStateAsString() })
   }
 
 
-  this.openCSV = function(url){
-    return new Promise(function(resolve, reject){
-      csv.loadFile(url).then( _=>{
+  this.openCSV = function (url) {
+    return new Promise(function (resolve, reject) {
+      csv.loadFile(url).then(_ => {
         setup()
-        setTimeout( () => bc.postMessage({'state': _this.exportStateAsString()}), 800)
+        setTimeout(() => bc.postMessage({ 'state': _this.exportStateAsString() }), 800)
         resolve()
       })
     })
   }
 
-  var openCSVfromState = function(url){
-    return new Promise(function(resolve, reject){
-      if (url === undefined){
+  var openCSVfromState = function (url) {
+    return new Promise(function (resolve, reject) {
+      if (url === undefined) {
         console.log('Error! Not loading any data into chart!')
         reject()
       }
-      csv.loadFile(url).then( _=>{
-        setup()        
+      csv.loadFile(url).then(_ => {
+        setup()
         resolve()
       })
     })
@@ -106,7 +106,7 @@ function Plotsvy(targetDiv) {
     if (state.plotAll) {
       _this.plotAll()
     } else {
-      if( headers.length < 100){ 
+      if (headers.length < 100) {
         ui.buildDatGui(exportObject)
         ui.createDatGuiDropdown(headers, checkBoxCall)
       } else {
@@ -114,85 +114,85 @@ function Plotsvy(targetDiv) {
         parentDiv.querySelector('#select_channel').onchange = csvChannelCall
         ui.buildDatGui(exportObject)
       }
-      if (!state.plotAll && state.selectedChannels.length === 0){
+      if (!state.plotAll && state.selectedChannels.length === 0) {
         _this.plotByIndex(1)
-        setTimeout(_this.updateSize, 500)  
+        setTimeout(_this.updateSize, 500)
       }
     }
   }
 
-  this.plotAll = function(){
+  this.plotAll = function () {
     plot.plotAll(csv.getAllData()) // plot all
     ui.hideSelector()
-    if (csv.getHeaders().length < 100){  
-      for (let i in ui.checkboxElements){
-          ui.checkboxElements[i].__checkbox.checked = true // update dat.gui
-      } 
+    if (csv.getHeaders().length < 100) {
+      for (let i in ui.checkboxElements) {
+        ui.checkboxElements[i].__checkbox.checked = true // update dat.gui
+      }
     }
-    setTimeout( _this.updateSize, 1000)
+    setTimeout(_this.updateSize, 1000)
     state.plotAll = true
   }
 
-  this.hideAll = function(){
+  this.hideAll = function () {
     _this.clearChart()
     _this.plotByIndex(1)
-    if (csv.getHeaders().length > 100){
+    if (csv.getHeaders().length > 100) {
       ui.showSelector()
     } else {
       ui.checkboxElements[0].__checkbox.checked = true
     }
-    setTimeout( _this.updateSize, 1000)     
+    setTimeout(_this.updateSize, 1000)
     state.plotAll = false
   }
 
 
-  this.plotByIndex = function(index){
+  this.plotByIndex = function (index) {
     var channelName = csv.getHeaderByIndex(index)
     plot.addDataSeriesToChart(csv.getColoumnByIndex(index), csv.getColoumnByIndex(0), channelName)
     state.selectedChannels.push(channelName)
   }
 
-  this.plotByNamePromise = function(channelName){
-    return new Promise(function(resolve, reject) {
+  this.plotByNamePromise = function (channelName) {
+    return new Promise(function (resolve, reject) {
       plot.addDataSeriesToChart(csv.getColoumnByName(channelName), csv.getColoumnByIndex(0), channelName)
       resolve()
     })
   }
 
-  this.plotByName = function(channelName){
+  this.plotByName = function (channelName) {
     plot.addDataSeriesToChart(csv.getColoumnByName(channelName), csv.getColoumnByIndex(0), channelName)
     state.selectedChannels.push(channelName)
   }
 
-  this.exportStateAsString = function(){
+  this.exportStateAsString = function () {
     return JSON.stringify(state)
   }
 
-  this.exportState = function(){
+  this.exportState = function () {
     return state
   }
 
 
-  this.setSubplotsFlag = function(flag){
-    plot.subplots = flag  
+  this.setSubplotsFlag = function (flag) {
+    plot.subplots = flag
     state.subplots = flag
   }
 
-  this.setDataType = function(dataType){
+  this.setDataType = function (dataType) {
     plot.plotType = dataType
     state.plotType = dataType
     ui.dataType = dataType
   }
 
-  this.loadState = function(jsonString){
-    return new Promise(function(resolve, reject){
+  this.loadState = function (jsonString) {
+    return new Promise(function (resolve, reject) {
       _this.clearChart()
       state.loadFromJSON(jsonString)
-      openCSVfromState(state.csvURL).then( _ => {
+      openCSVfromState(state.csvURL).then(_ => {
         plot.plotType = state.plotType
         plot.subplots = state.subplots
-        if (state.selectedChannels !== undefined){
-          if (state.selectedChannels.length > 0){
+        if (state.selectedChannels !== undefined) {
+          if (state.selectedChannels.length > 0) {
             if (!state.plotAll) {
               plotStateChannels(state.selectedChannels)
             }
@@ -204,18 +204,18 @@ function Plotsvy(targetDiv) {
     })
   }
 
-  var plotStateChannels = function(channels){
+  var plotStateChannels = function (channels) {
     _this.plotByNamePromise(channels[0]).then(_ => {
-      for (let i = 0; i <channels.length; i++){
-        if (i === 0){
+      for (let i = 0; i < channels.length; i++) {
+        if (i === 0) {
           continue
         }
         _this.plotByNamePromise(channels[i])
       }
     })
-    for (let i in channels){
-      for (let j in ui.checkboxElements){
-        if (ui.checkboxElements[j].property === channels[i]){
+    for (let i in channels) {
+      for (let j in ui.checkboxElements) {
+        if (ui.checkboxElements[j].property === channels[i]) {
           ui.checkboxElements[j].__checkbox.checked = true
           break
         }
@@ -223,30 +223,18 @@ function Plotsvy(targetDiv) {
     }
   }
 
-  // ------------ SIM UI Management ------------------
+  // ------------ External Calls ------------------
 
-  this.listenOn = function(name){
-    _this.bc2 = new BroadcastChannel.default(name)
-    _this.bc2.onmessage = (ev) => processResults(ev)
+  this.addDataSeriesToChart = function (newSeries, xaxis, id) {
+    plot.addDataSeriesToChart(newSeries, xaxis, id)
   }
 
-  var processResults = function(event){
-    data = JSON.parse(event.data.data)
-    sampleRate = data.sampleRate
-    ydata = data.y
-    xaxis = []
-    for (let i = 0; i < ydata.length; i++){
-      xaxis.push(i*sampleRate)
-    }
-    plot.addDataSeriesToChart(ydata, xaxis, 'results')
-  }
-
-  this.initialiseForSim = function(){
-    ui.createSimDatGui(exportObject)
+  this.exportObject = function () {
+    return exportObject
   }
   // --------------------------------------------------
 
-  this.clearChart = function(){
+  this.clearChart = function () {
     plot.resetChart()
     state.selectedChannels = []
   }
@@ -257,14 +245,14 @@ function Plotsvy(targetDiv) {
     setup()
   }
 
-  this.exportCSV = function(){
+  this.exportCSV = function () {
     csv.export(state)
   }
 
-  this.exportToOpenCOR = function(){
+  this.exportToOpenCOR = function () {
     csv.exportToOpenCOR(state)
   }
-  var exportObject = {      
+  var exportObject = {
     'Export as CSV': () => csv.export(state),
     'Open in OpenCOR': () => csv.exportForOpenCOR(state),
     'Show All': () => _this.plotAll(),
@@ -272,7 +260,7 @@ function Plotsvy(targetDiv) {
     'Switch Axes': () => _this.switchAxes()
   }
 
-  this.updateSize = function(){
+  this.updateSize = function () {
     var dataset_div = parentDiv.querySelector('#dataset_div')
     var chart_height = parentDiv.clientHeight - dataset_div.offsetHeight
     plot.resizePlot(parentDiv.clientWidth, chart_height)
