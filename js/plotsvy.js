93aedbc3e84309a6772b0cdb874474dd00667b7f
Jesse Khorasanee <jessekhorasanee@gmail.com>
2019-11-27 12:19:38 +1300

    Swtich to using ES6 classes

--- js/plotsvy.js
+++ js/plotsvy.js
@@ -15,107 +15,124 @@ const plotsvy_html = require('.././snippets/plotsvy.html')
 
 // Plotsvy:   Manages the interactions between modules.
 //  param: targetDiv - the div container the plot ends up in
-function Plotsvy(targetDiv, inputURL) {
-  var ui = undefined
-  var parentDiv = undefined
-  var plot = undefined
-  var csv = undefined
-  var state = undefined
-  var fileNav = undefined
-  var dirString = 'directory-meta'
-  var _this = this
-  var bc = new BroadcastChannel.default('plot_channel') // For updating other web apps working with Plotsvy
-  _this.plot = plot
+class Plotsvy {
+  constructor(targetDiv, inputURL) {
+    var ui = undefined
+    var parentDiv = undefined
+    var plot = undefined
+    var csv = undefined
+    var state = undefined
+    var fileNav = undefined
+    var dirString = 'directory-meta'
+    var bc = new BroadcastChannel.default('plot_channel') // For updating other web apps working with Plotsvy
 
-  // If targetDiv is not provided, we will assume it already exists (this is used for the demo)
-  if (targetDiv === null || targetDiv === undefined) {
-    parentDiv = document.getElementById('plotsvy-example-panel')
-  } else {
-    parentDiv = targetDiv
-  }
+    var datguiTimeseriesFunctions = {
+      'Export as CSV': () => this.csv.export(this.state),
+      'Open in OpenCOR': () => this.csv.exportForOpenCOR(this.state),
+      'Show All': () => this.plotAll(),
+      'Hide All': () => this.hideAll(),
+      'Switch Axes': () => this.switchAxes()
+    }
+  
+    var datguiStaticFunctions = {
+      'Export as CSV': () => this.csv.export(this.state),
+      'Open in OpenCOR': () => this.csv.exportForOpenCOR(this.state),
+      'Show All': () => this.plotAll(),
+      'Hide All': () => this.hideAll(),
+      'Switch Axes': () => this.switchAxes(),
+      'Plot as Heatmap': () => this.heatMapPlotSwitch()
+    }
+
+    // If targetDiv is not provided, we will assume it already exists (this is used for the demo)
+    if (targetDiv === null || targetDiv === undefined) {
+      this.parentDiv = document.getElementById('plotsvy-example-panel')
+    } else {
+      this.parentDiv = targetDiv
+    }
+    this.initialise()
+  } 
 
 
   // initialise: Sets up ui and plot, requires DOM to be loaded (Note: Called on construction)
-  this.initialise = function (overRideUrl) {
-    parentDiv.append(htmlToElement(plotsvy_html))
-    var chartDiv = parentDiv.querySelector('#chart_div')
-    ui = new UI(parentDiv)
-    plot = new PlotManager(chartDiv)
-    csv = new CsvManager()
-    _this.csv = csv
-    state = new StateManager(parentDiv)
-    _this.state = state
+  initialise(overRideUrl) {
+    this.parentDiv.append(htmlToElement(plotsvy_html))
+    var chartDiv = this.parentDiv.querySelector('#chart_div')
+    this.ui = new UI(this.parentDiv)
+    this.plot = new PlotManager(chartDiv)
+    this.csv = new CsvManager()
+    this.state = new StateManager(this.parentDiv)
+
 
     // Check if we have have a URL yet
     if (overRideUrl !== undefined){
-      _this.openInputUrl(inputURL)
+      this.openInputUrl(inputURL)
     } else if (inputURL !== undefined){
-      _this.openInputUrl(inputURL)
+      this.openInputUrl(inputURL)
     }
   }
 
   // openInputUrl: Opens url in directory or single file mode based on urlstring
-  this.openInputUrl = function(url){
+  openInputUrl(url){
 
     // Multi File
     if (url.includes(dirString)){ 
-      ui.showLoadingGif()
-      ui.showDirectoryContent()
-      _this.createFileNavigation(url).then( _=>{
-        ui.hideLoadingGif()
+      this.ui.showLoadingGif()
+      this.ui.showDirectoryContent()
+      this.createFileNavigation(url).then( _=>{
+        this.ui.hideLoadingGif()
       })
     
     // Single File
     } else { 
-      ui.showLoadingGif()
-      ui.hideDirectoryContent()
-      _this.openCSV(url).then(ui.hideLoadingGif)
+      this.ui.showLoadingGif()
+      this.ui.hideDirectoryContent()
+      this.openCSV(url).then(this.ui.hideLoadingGif)
     }
   }
 
-  this.openBroadcastChannel = function (name) {
+  openBroadcastChannel (name) {
     bc.close()
     bc = new BroadcastChannel.default(name)
   }
 
-  this.sendChannelMessage = function (message) {
+  sendChannelMessage (message) {
     bc.postMessage(message)
   }
 
   // csvChannelCall: Adds a trace to the plot from 'select_channel' div
-  var csvChannelCall = function () {
-    var selectedChannel = parentDiv.querySelector('#select_channel').textContent
-    plot.addDataSeriesToChart(csv.getColoumnByName(selectedChannel), csv.getColoumnByIndex(0), selectedChannel)
-    state.selectedChannels.push(selectedChannel)
-    bc.postMessage({ 'state': _this.exportStateAsString() })
+  csvChannelCall () {
+    var selectedChannel = this.parentDiv.querySelector('#select_channel').textContent
+    this.plot.addDataSeriesToChart(this.csv.getColoumnByName(selectedChannel), this.csv.getColoumnByIndex(0), selectedChannel)
+    this.state.selectedChannels.push(selectedChannel)
+    bc.postMessage({ 'state': this.exportStateAsString() })
   }
 
   // checkBoxCall: Function to pass to dat.gui to add trace to plot
-  var checkBoxCall = function (channel, index, flag) {
+  checkBoxCall (channel, index, flag) {
     if (!flag) { // Flag lets us know if checkbox was checked before this click
-      plot.addDataSeriesFromDatGui(csv.getColoumnByIndex(index), csv.getColoumnByIndex(0), channel, index)
-      state.selectedChannels.push(channel)
+      this.plot.addDataSeriesFromDatGui(this.csv.getColoumnByIndex(index), this.csv.getColoumnByIndex(0), channel, index)
+      this.state.selectedChannels.push(channel)
     }
     else {
-      plot.removeSeries(index)
-      ch_ind = state.selectedChannels.indexOf(channel)
-      state.selectedChannels.splice(ch_ind, ch_ind + 1) // Removes channel index
+      this.plot.removeSeries(index)
+      ch_ind = this.state.selectedChannels.indexOf(channel)
+      this.state.selectedChannels.splice(ch_ind, ch_ind + 1) // Removes channel index
     }
-    bc.postMessage({ 'state': _this.exportStateAsString() })
+    bc.postMessage({ 'state': this.exportStateAsString() })
   }
 
 
-  this.openCSV = function (url) {
+  openCSV (url) {
     return new Promise(function (resolve, reject) {
-      _this.clearChart()
-      ui.showLoadingGif()
-      csv.loadFile(url).then(_ => {
-        ui.setTitle(csv.getTitle(url))
+      this.clearChart()
+      this.ui.showLoadingGif()
+      this.csv.loadFile(url).then(_ => {
+        this.ui.setTitle(this.csv.getTitle(url))
         setup()
-        state.csvURL = url
-        setTimeout(() => bc.postMessage({ 'state': _this.exportStateAsString() }), 800)
-        ui.hideLoadingGif()
-        parentDiv.querySelectorAll('.multi-file')[1].style.display = 'none'
+        this.state.csvURL = url
+        setTimeout(() => bc.postMessage({ 'state': this.exportStateAsString() }), 800)
+        this.ui.hideLoadingGif()
+        this.parentDiv.querySelectorAll('.multi-file')[1].style.display = 'none'
         if(fileNav !== undefined){
           // fileNav.collapseAll()
         }
@@ -124,143 +141,143 @@ function Plotsvy(targetDiv, inputURL) {
     })
   }
 
-  var openCSVfromState = function (url) {
+  openCSVfromState (url) {
     return new Promise(function (resolve, reject) {
-      ui.showLoadingGif()
+      this.ui.showLoadingGif()
       if (url === undefined) {
         console.log('Error! Not loading any data into chart!')
         reject()
       }
-      csv.loadFile(url).then(_ => {
-        ui.setTitle(csv.getTitle(url))
+      this.csv.loadFile(url).then(_ => {
+        this.ui.setTitle(this.csv.getTitle(url))
         setup()
-        ui.hideLoadingGif()
+        this.ui.hideLoadingGif()
         resolve()
       })
     })
   }
 
   // setup: Creates different UI's depending on type of data and plots data depending on state.plotall 
-  var setup = function () {
-    plot.setXaxisLabel(csv.getXaxis())
-    _this.setDataType(csv.getDataType())
-    ui.showSelector()
+  setup () {
+    this.plot.setXaxisLabel(this.csv.getXaxis())
+    this.setDataType(this.csv.getDataType())
+    this.ui.showSelector()
     
-    var headers = [...csv.getHeaders()]
+    var headers = [...this.csv.getHeaders()]
     headers.shift()
-    if (state.plotAll) {
-      _this.plotAll()
+    if (this.state.plotAll) {
+      this.plotAll()
     } else {
 
       // Check which functions we need for datgui
       var datguiFunctions = datguiStaticFunctions
-      if (csv.getDataType() === 'scatter'){
+      if (this.csv.getDataType() === 'scatter'){
         datguiFunctions = datguiTimeseriesFunctions
       }
 
       // Dat.gui UI
       if (headers.length < 100) {
-        ui.buildDatGui(datguiFunctions)
-        ui.createDatGuiDropdown(headers, checkBoxCall)
+        this.ui.buildDatGui(datguiFunctions)
+        this.ui.createDatGuiDropdown(headers, checkBoxCall)
       } 
 
       // Select2 UI for navigating large amounts of headers
       else { 
-        ui.createSelectDropdown(headers)
-        parentDiv.querySelector('#select_channel').onchange = csvChannelCall
-        ui.buildDatGui(datguiFunctions)
+        this.ui.createSelectDropdown(headers)
+        this.parentDiv.querySelector('#select_channel').onchange = csvChannelCall
+        this.ui.buildDatGui(datguiFunctions)
       }
-      if (!state.plotAll && state.selectedChannels.length === 0) {
-        _this.plotByIndex(1)
-        setTimeout(_this.updateSize, 500)
+      if (!this.state.plotAll && this.state.selectedChannels.length === 0) {
+        this.plotByIndex(1)
+        setTimeout(this.updateSize, 500)
       }
     }
   }
 
   // createFileNavigation: Uses a meta-data url to create navigation
-  this.createFileNavigation = function(url){
+  createFileNavigation(url){
     return new Promise(function(resolve, reject){
-      var fileNavDiv = parentDiv.querySelector('#file_nav')
-      fileNav = new FileManager(fileNavDiv, url, _this.openCSV)
+      var fileNavDiv = this.parentDiv.querySelector('#file_nav')
+      fileNav = new FileManager(fileNavDiv, url, this.openCSV)
       resolve()
     })
   }
 
-  this.plotAll = function () {
-    plot.plotAll(csv.getAllData()) // plot all
-    ui.hideSelector()
-    if (csv.getHeaders().length < 100) {
-      for (let i in ui.checkboxElements) {
-        ui.checkboxElements[i].__checkbox.checked = true // update dat.gui
+  plotAll () {
+    this.plot.plotAll(this.csv.getAllData()) // plot all
+    this.ui.hideSelector()
+    if (this.csv.getHeaders().length < 100) {
+      for (let i in this.ui.checkboxElements) {
+        this.ui.checkboxElements[i].__checkbox.checked = true // update dat.gui
       }
     }
-    setTimeout(_this.updateSize, 1000)
-    state.plotAll = true
+    setTimeout(this.updateSize, 1000)
+    this.state.plotAll = true
   }
 
-  this.hideAll = function () {
-    _this.clearChart()
-    if (csv.getHeaders().length > 100) {
-      ui.showSelector()
+  hideAll () {
+    this.clearChart()
+    if (this.csv.getHeaders().length > 100) {
+      this.ui.showSelector()
     } 
-    setTimeout(_this.updateSize, 1000)
-    state.plotAll = false
+    setTimeout(this.updateSize, 1000)
+    this.state.plotAll = false
   }
 
 
-  this.plotByIndex = function (index) {
-    var channelName = csv.getHeaderByIndex(index)
-    plot.addDataSeriesToChart(csv.getColoumnByIndex(index), csv.getColoumnByIndex(0), channelName)
-    state.selectedChannels.push(channelName)
+  plotByIndex (index) {
+    var channelName = this.csv.getHeaderByIndex(index)
+    this.plot.addDataSeriesToChart(this.csv.getColoumnByIndex(index), this.csv.getColoumnByIndex(0), channelName)
+    this.state.selectedChannels.push(channelName)
   }
 
-  this.plotByNamePromise = function (channelName) {
+  plotByNamePromise (channelName) {
     return new Promise(function (resolve, reject) {
-      searchResult = csv.getColoumnByName(channelName)
+      searchResult = this.csv.getColoumnByName(channelName)
       if (searchResult === false){
         console.log('Could not find channel: "'+ channelName +'" in the loaded csv file')
         reject()
       } else {
-        plot.addDataSeriesToChart(searchResult, csv.getColoumnByIndex(0), channelName)
+        this.plot.addDataSeriesToChart(searchResult, this.csv.getColoumnByIndex(0), channelName)
         resolve()
       }
     })
   }
 
-  this.plotByName = function (channelName) {
-    searchResult = csv.getColoumnByName(channelName)
+  plotByName (channelName) {
+    searchResult = this.csv.getColoumnByName(channelName)
     if (searchResult === false){
       console.log('Could not find channel: "'+ channelName +'" in the loaded csv file')
       return false
     }
-    plot.addDataSeriesToChart(searchResult, csv.getColoumnByIndex(0), channelName)
-    state.selectedChannels.push(channelName)
+    this.plot.addDataSeriesToChart(searchResult, this.csv.getColoumnByIndex(0), channelName)
+    this.state.selectedChannels.push(channelName)
   }
 
-  this.exportStateAsString = function () {
-    return JSON.stringify(state)
+  exportStateAsString () {
+    return JSON.stringify(this.state)
   }
 
-  this.exportState = function () {
-    return state
+  exportState () {
+    return this.state
   }
 
-  this.setDataType = function (dataType) {
-    plot.plotType = dataType
-    state.plotType = dataType
-    ui.dataType = dataType
+  setDataType (dataType) {
+    this.plot.plotType = dataType
+    this.state.plotType = dataType
+    this.ui.dataType = dataType
   }
 
-  this.loadState = function (jsonString) {
+  loadState (jsonString) {
     return new Promise(function (resolve, reject) {
-      _this.clearChart()
-      state.loadFromJSON(jsonString)
-      openCSVfromState(state.csvURL).then(_ => {
-        plot.plotType = state.plotType
-        if (state.selectedChannels !== undefined) {
-          if (state.selectedChannels.length > 0) {
-            if (!state.plotAll) {
-              plotStateChannels(state.selectedChannels)
+      this.clearChart()
+      this.state.loadFromJSON(jsonString)
+      openCSVfromState(this.state.csvURL).then(_ => {
+        this.plot.plotType = this.state.plotType
+        if (this.state.selectedChannels !== undefined) {
+          if (this.state.selectedChannels.length > 0) {
+            if (!this.state.plotAll) {
+              plotStateChannels(this.state.selectedChannels)
             }
           }
         }
@@ -270,24 +287,24 @@ function Plotsvy(targetDiv, inputURL) {
     })
   }
 
-  var plotStateChannels = function (channels) {
+  plotStateChannels (channels) {
     if (Array.isArray(channels) === false){ //check if channels is string
-      _this.plotByNamePromise(channels)
-      state.selectedChannels = [state.selectedChannels] //if channels aren't in array make one
+      this.plotByNamePromise(channels)
+      this.state.selectedChannels = [this.state.selectedChannels] //if channels aren't in array make one
     } else {
-      _this.plotByNamePromise(channels[0]).then(_ => { //allow first plot to finish
+      this.plotByNamePromise(channels[0]).then(_ => { //allow first plot to finish
         for (let i = 0; i < channels.length; i++) {
           if (i === 0) {
             continue
           }
-          _this.plotByNamePromise(channels[i]) //add remaining channels
+          this.plotByNamePromise(channels[i]) //add remaining channels
         }
       })
       // Update dat gui via search
       for (let i in channels) {
-        for (let j in ui.checkboxElements) {
-          if (ui.checkboxElements[j].property === channels[i]) {
-            ui.checkboxElements[j].__checkbox.checked = true
+        for (let j in this.ui.checkboxElements) {
+          if (this.ui.checkboxElements[j].property === channels[i]) {
+            this.ui.checkboxElements[j].__checkbox.checked = true
             break
           }
         }
@@ -295,70 +312,52 @@ function Plotsvy(targetDiv, inputURL) {
     }
   }
 
-  this.clearChart = function () {
-    plot.resetChart()
-    state.selectedChannels = []
+  clearChart () {
+    this.plot.resetChart()
+    this.state.selectedChannels = []
   }
 
-  this.switchAxes = function () {
-    _this.clearChart()
-    csv.transposeSelf()
+  switchAxes () {
+    this.clearChart()
+    this.csv.transposeSelf()
     setup()
   }
 
-  this.exportCSV = function () {
-    csv.export(state)
+  exportCSV () {
+    this.csv.export(this.state)
   }
 
-  this.exportToOpenCOR = function () {
-    csv.exportToOpenCOR(state)
+  exportToOpenCOR () {
+    this.csv.exportToOpenCOR(this.state)
   }
 
-  var datguiTimeseriesFunctions = {
-    'Export as CSV': () => csv.export(state),
-    'Open in OpenCOR': () => csv.exportForOpenCOR(state),
-    'Show All': () => _this.plotAll(),
-    'Hide All': () => _this.hideAll(),
-    'Switch Axes': () => _this.switchAxes()
-  }
 
-  var datguiStaticFunctions = {
-    'Export as CSV': () => csv.export(state),
-    'Open in OpenCOR': () => csv.exportForOpenCOR(state),
-    'Show All': () => _this.plotAll(),
-    'Hide All': () => _this.hideAll(),
-    'Switch Axes': () => _this.switchAxes(),
-    'Plot as Heatmap': () => _this.heatMapPlotSwitch()
-  }
-
-  this.heatMapPlotSwitch = function() {
-    if (_this.state.plotType === 'bar'){
-      _this.heatMapPlot()
-      _this.state.plotType = 'heatmap'
+  heatMapPlotSwitch() {
+    if (this.state.plotType === 'bar'){
+      this.heatMapPlot()
+      this.state.plotType = 'heatmap'
     } else {
-      plot.resetChart()
-      _this.plotByIndex(1)
-      _this.state.plotType = 'bar'
+      this.plot.resetChart()
+      this.plotByIndex(1)
+      this.state.plotType = 'bar'
     }
-    ui.switchBarHeatmapButton()
+    this.ui.switchBarHeatmapButton()
   }
 
-  this.heatMapPlot = function(){
-    var nested_rows = csv.getAllData()
-    var y_headers = csv.getHeaders()
-    var x_headers = csv.getColoumnByIndex(0)
+  heatMapPlot(){
+    var nested_rows = this.csv.getAllData()
+    var y_headers = this.csv.getHeaders()
+    var x_headers = this.csv.getColoumnByIndex(0)
     y_headers.shift()
     x_headers.shift()
-    plot.heatMapPlot(nested_rows, x_headers, y_headers)
+    this.plot.heatMapPlot(nested_rows, x_headers, y_headers)
   }
 
-  this.updateSize = function () {
-    var dataset_div = parentDiv.querySelector('#dataset_div')
-    var chart_height = parentDiv.clientHeight - dataset_div.offsetHeight
-    plot.resizePlot(parentDiv.clientWidth, chart_height)
+  updateSize () {
+    var dataset_div = this.parentDiv.querySelector('#dataset_div')
+    var chart_height = this.parentDiv.clientHeight - dataset_div.offsetHeight
+    this.plot.resizePlot(this.parentDiv.clientWidth, chart_height)
   }
-
-  _this.initialise()
 }
 
 htmlToElement = (html) => {
